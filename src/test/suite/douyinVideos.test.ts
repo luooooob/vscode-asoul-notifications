0f21b0e7d34fc3b5fed47de323a032ed46763ae9
Jiang Yan <contact@jiang-yan.com>
2021-09-26 19:27:14 +0800

    Add douyinVideos test

---
+++ src/test/suite/douyinVideos.test.ts
@@ -0,0 +1,12 @@
+import * as assert from 'assert';
+import axios from 'axios';
+import { requestVideos } from "../../douyinVideos";
+import { BilibiliDynamic, BilibiliDynamicsResponse } from '../../types';
+
+suite('DouyinVideos Functions Test Suite', () => {
+
+  test('Test requestDouyinVideos', async () => {
+  });
+
+
+});
